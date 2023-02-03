d0bf7db930fbd74b3aa0e0a96a172ebff023334e
LisaLyakh <99537140+LisaLyakh@users.noreply.github.com>
2023-02-03 14:19:11 +0200

    add

---
+++ controller/users/login.js
@@ -0,0 +1,31 @@
+const { User } = require("../../models/user");
+const { Unauthorized } = require("http-errors");
+const jwt = require("jsonwebtoken");
+const { SECRET_KEY } = process.env;
+
+const login = async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user || !user.comparePassword(password)) {
+        throw new Unauthorized("Email or password is wrong");
+    }
+
+    const payload = {
+        id: user._id,
+    };
+    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "5h" });
+    await User.findByIdAndUpdate(user._id, { token });
+    res.json({
+        status: "success",
+        code: 200,
+        data: {
+            token,
+            user: {
+                email,
+                subscription: user.subscription,
+            },
+        },
+    });
+};
+
+module.exports = login;
---
+++ routes/api/users.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const { schemas } = require("../../models/user");
+const { ctrlWrapper, auth, validation } = require("../../middlewares");
+const {
+    register,
+    login,
+    currentUser,
+    logout,
+    updateSubscription,
+} = require("../../controller");
+
+const router = express.Router();
+
+router.post("/signup", validation(schemas.register), ctrlWrapper(register));
+router.post("/login", validation(schemas.login), ctrlWrapper(login));
+router.get("/current", auth, ctrlWrapper(currentUser));
+router.get("/logout", auth, ctrlWrapper(logout));
+router.patch(
+    "/:id/subscription",
+    auth,
+    validation(schemas.subscription),
+    ctrlWrapper(updateSubscription)
+);
+
+module.exports = router;
